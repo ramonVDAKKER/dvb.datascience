acaf81366fee6df74976aca5b95c6bbb350e54bd
Marc Rijken <marc.rijken@sns.nl>
2018-10-22 20:06:05 +0200

    Improve smote docs

--- dvb/datascience/transform/smote.py
+++ dvb/datascience/transform/smote.py
@@ -10,6 +10,25 @@ class SMOTESampler(ClassificationPipeBase):
 
     Note: the new df will not the indexes, because of extra creates row, the indexes
     won't be unique anymore.
+
+    The constructor will pass all kwargs to SMOTE, so all
+    arguments of SMOTE can be used. For example:
+
+    ratio (default: 'auto'):
+        - If ``str``, has to be one of: (i) ``'minority'``: resample the
+          minority class; (ii) ``'majority'``: resample the majority class,
+          (iii) ``'not minority'``: resample all classes apart of the minority
+          class, (iv) ``'all'``: resample all classes, and (v) ``'auto'``:
+          correspond to ``'all'`` with for over-sampling methods and ``'not
+          minority'`` for under-sampling methods. The classes targeted will be
+          over-sampled or under-sampled to achieve an equal number of sample
+          with the majority or minority class.
+        - If ``dict``, the keys correspond to the targeted classes. The values
+          correspond to the desired number of samples.
+        - If callable, function taking ``y`` and returns a ``dict``. The keys
+          correspond to the targeted classes. The values correspond to the
+          desired number of samples.
+
     """
 
     input_keys = ("df", "df_metadata")
